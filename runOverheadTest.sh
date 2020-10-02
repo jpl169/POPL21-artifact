3f8f59750ca08830f02112bdbc27662061c8ee52
Jay P Lim <jay@Jays-MacBook-Pro-2.local>
2020-10-02 14:52:19 -0500

    Formatting output file to be more readable

--- runOverheadTest.sh
+++ runOverheadTest.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-echo "\033[1mBuilding math libraries\033[0m"
+ResultPath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ResultPath="${ResultPath}/OverheadTestResult.txt"
+
+echo -e "\033[1mBuilding math libraries\033[0m"
 make -s clean
 make -s
 
-echo "\033[1mMeasuring bfloat16 math library speed\033[0m"
+echo -e "\033[1mMeasuring bfloat16 math library speed\033[0m"
+echo -e "Measuring bfloat16 math library speed" >> $ResultPath
 cd overheadtest/bfloat16
 make -s clean
 make -s
-./runAll.sh
+./runAll.sh >> $ResultPath
 make -s clean
+echo -e "" >> $ResultPath
+echo -e "" >> $ResultPath
 
-echo "\033[1mMeasuring posit16 math library speed\033[0m"
+echo -e "\033[1mMeasuring posit16 math library speed\033[0m"
+echo -e "Measuring posit16 math library speed" >> $ResultPath
 cd ../posit16
 make -s clean
 make -s
-./runAll.sh
+./runAll.sh >> $ResultPath
 make -s clean
+echo -e "" >> $ResultPath
+echo -e "" >> $ResultPath
 
-echo "\033[1mMeasuring float math library speed\033[0m"
+echo -e "\033[1mMeasuring float math library speed\033[0m"
+echo -e "Measuring float math library speed" >> $ResultPath
+echo -e "This make 10-20 minutes to complete"
 cd ../float
 make -s clean
 make -s
-./runAll.sh
+./runAll.sh >> $ResultPath
 make -s clean
 
 cd ../..
